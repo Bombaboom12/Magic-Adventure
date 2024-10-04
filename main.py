a61f759f4e8a1a783d06fa0aff1232bf807fa25a
slvva <pignagnolisilvia@gmail.com>
2024-10-04 17:21:50 +0200

    Images/Mago/Idle.png

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from settings import *
 from player import Player
+from groups import AllSprites
+from settore import *
+from pytmx.util_pygame import load_pygame
 
 pygame.init()
 
 class Game:
+
     def __init__(self):
-        self.screen = pygame.display.set_mode((LARGHEZZA, ALTEZZA))
+        
+        self.display_surf = pygame.display.set_mode((LARGHEZZA, ALTEZZA))
         pygame.display.set_caption("Magic Adventure")
         self.clock = pygame.time.Clock()
         self.running = True
 
+        self.all_sprites = AllSprites()
+        self.player = Player((300, 700), self.all_sprites)
+        self.setup()
+
+    def setup(self):
+        map1 = load_pygame(join(".tiled", "Hub.tmx"))
+        map2 = load_pygame(join(".tiled", "Hub2.tmx"))
+        
+        for x, y, image in map1.get_layer_by_name("Livello tile 1").tiles():
+            Settore((x * 16, y * 16), image, self.all_sprites)
+        
     def run(self):
         while self.running:
+
             deltaTime = self.clock.tick() / 1000
     
             # Event loop
@@ -20,8 +37,8 @@ class Game:
                     self.running = False
 
             # Draw the game
-            self.screen.fill("Blue")
-
+            self.all_sprites.update(deltaTime)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 
         pygame.quit()
--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = 
+        self.image = pygame.image.load(join("Images", "Mago", "Idle.png"))
         self.rect = self.image.get_frect(center = pos)
 
         # Movement 
@@ -14,11 +14,17 @@ class Player(pygame.sprite.Sprite):
     def input(self):
         keys = pygame.key.get_pressed()
         self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
+        
+        recent_keys = pygame.key.get_just_pressed()
+        if recent_keys[pygame.K_SPACE]:
+            self.direction.y -= 1
+
         self.direction = self.direction.normalize() if self.direction else self.direction
 
     def move(self, deltaTime):
-        pass
-
+        self.rect.x += self.direction.x * self.speed * deltaTime
+        self.rect.y += self.direction.y * self.speed * deltaTime
+        
     def collision(self, direction):
         pass
 
